3c9d58ded6d9f09214b901d21bd618b41e4c58b3
Ben Stager <bstager@tulane.edu>
2023-03-08 14:32:31 -0500

    changed inverse matrix, IMEXmidpoint

--- pde/integrators/IMEXmidpoint.m
+++ pde/integrators/IMEXmidpoint.m
@@ -6,15 +6,19 @@ ys = zeros(length(y0),N+1);
 ys(:,1) = y0;
 y = y0;
 dt = diff(tspan)/N;
-I = speye(length(y0));
+
+if(issparse(L))
+    I = speye(length(y0));
+else
+    I = eye(length(y0));
+end
 
 tic
 for i = 1:N
     k1_hat = Nl(y);
-    k1 = L*(I-dt/2*L)^-1*(y+dt/2*k1_hat);
+    k1 = L*((I-dt/2*L)\(y+dt/2*k1_hat));
     k2_hat = Nl(y+dt/2*(k1+k1_hat));
     y = y+dt*(k1+k2_hat);
-    % y = y+dt*(L*(I-dt/2*L)^-1*(y+dt/2*Nl(y)) + Nl(y+dt/2*(L*(I-dt/2*L)^-1*(y+dt/2*Nl(y)))+dt/2* Nl(y)));
     ys(:,i+1) = y;
 end
 cpu_time = toc;
