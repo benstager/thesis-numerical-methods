6721c3a792c70266628628189f71147b56e1e7a8
Ben Stager <bstager@tulane.edu>
2023-03-14 02:14:46 -0400

    changed parent names, added stokes folder

---
+++ Time integration/stokes/forces.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = forces(inputArg1,inputArg2)
+%FORCES Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+x = 1;
+end
+
