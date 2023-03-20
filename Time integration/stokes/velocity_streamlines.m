8ec1a8bd01d8c8f15564173fc93ad7bb753158dc
Ben Stager <bstager@tulane.edu>
2023-03-20 16:46:17 -0600

    attempted streamline function

--- Time integration/stokes/velocity_streamlines.m
+++ Time integration/stokes/velocity_streamlines.m
@@ -1,13 +1,26 @@
 
-% Function produces a phase plane in (x(t),y(t), solving dynamical system
 
-% How do we find the solution to [vx;vy] = [f(x,y); g(x,y)]
-% Y' = AY
-% Can we return a functional handle for each vx and vy and go that way?
+% Function should produce streamline curves in the parametrized x-y plane
+
+% Setting initial values
+ny = 5;
+nx = 5;
+[xks,fks,xs,ys] = stokes_parameters(ny,nx);
 
 
+% Producing a velocity vector at certain X = (x;y) position
 
+y0 = [.3;.5];
+% F = @(t,X) velocity(X,xks,fks);
 
-[xks,fks] = stokes_parameters();
-F = @(t,X) velocity(X,xks,fks);
+for i = 1:nx
+    for j = 1:ny
+        v = velocity([xs(i),ys(j)],xks,fks);
+        [ysx,cpux] = eulerLin(v(1),[xs(1),xs(end)],y0(1),nx);
+        [ysy,cpuy] = eulerLin(v(2),[ys(1),ys(end)],y0(2),ny);
+        plot(ysx,ysy);
+        hold on;
+    end
+end
 
+% Producing garbage currently
