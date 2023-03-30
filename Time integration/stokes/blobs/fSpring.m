6439d74e83a4e378b00b3cb87b3e21574dd8ae19
Ben Stager <bstager@tulane.edu>
2023-03-30 17:02:39 -0400

    sequestered spring force into separate file

---
+++ Time integration/stokes/blobs/fSpring.m
@@ -0,0 +1,12 @@
+function [F] = fSpring(Xv,i,j)
+% returns spring force on blob i, from j
+
+k = 1000; % spring constant
+len_rest = .1;
+len_curr = norm(Bp(Xv,j)- Bp(Xv,i),2);
+f_mag = k*(len_curr-len_rest);
+unit_vector = (Bp(Xv,j) - Bp(Xv,i))/len_curr;
+F = f_mag * unit_vector;
+
+end
+
