0c588ad78fa49e7c76e45d099e03451bb741d0fe
Ben Stager <bstager@tulane.edu>
2023-04-04 18:18:59 -0400

    numerical jacobian given system of equations

---
+++ Time integration/jacobian/FD_JX.m
@@ -0,0 +1,19 @@
+function J = FD_JX(X,h)
+
+% function returns the Jacobian given a system F and a vector X, stepsize h
+
+F = {@(x,y) 2*x^2 - 3*y^2; @(x,y) 3*x^2 - 4*y^2};
+J = zeros(size(F,1),length(X));
+
+for i = 1:length(X)
+    for j = 1:size(F,1)
+        e = zeros(length(X),1);
+        e(i) = 1;
+        X_new = X + h*e;
+        J(j,i) = (F{j}(X_new(1), X_new(2)) - F{j}(X(1),X(2)))/h;
+    end
+end
+
+
+end
+
