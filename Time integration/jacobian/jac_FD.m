b81d6332b2085fe3d312465d170ecda68d2233ec
Ben Stager <bstager@tulane.edu>
2023-04-04 19:00:14 -0400

    changed J initilization

--- Time integration/jacobian/jac_FD.m
+++ Time integration/jacobian/jac_FD.m
@@ -2,6 +2,7 @@ function J = jac_FD(F,X,h)
 
 % computes numerical jacobian at point X in R^j for small h
 % for function F, with f_1...f_i
+J = zeros(length(X),length(X));
 
 for i = 1:length(X)
     e = zeros(length(X),1);
