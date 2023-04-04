50942b6a89e2b241c649ea432d776c04232c92df
Ben Stager <bstager@tulane.edu>
2023-04-03 21:49:34 -0400

    changed code readability

--- Numerical solutions/oneDHeat.m
+++ Numerical solutions/oneDHeat.m
@@ -1,52 +1,40 @@
-function [UN,x,t] = oneDHeat()
+
 clear; clc; 
 % Inital data
-nx = 200;
-nt = 20000;
-a = -1;
-b = 1;
-t0 = 0;
-t1 = 1;
+Nx = 200;
+Nt = 20000;
+xspan = [-1,1];
+tspan = [0,.1];
 
 % Time and spatial steps
-dx = (b-a)/(nx - 1);
-dt = (t1 - t0)/(nt - 1); 
+dx = diff(xspan)/(Nx-1);
+dt = diff(tspan)/(Nt-1); 
 
 % Each x and t vectors to evaluate u(x,t) at
-x = a:dx:b;
-t = t0:dt:t1;
+xs = linspace(xspan(1),xspan(end),Nx);
+ts = linspace(xspan(1),xspan(end),Nt);
 
 % Coeffecient of finite approximation
 alpha = dt/dx^2;
 
-
-% NUMERICAL SOLUTION
-
-
 % Grid
-UN = zeros(nx,nt);
+u = zeros(Nx,Nt);
 
-
-% Initial conditions
-
-UN(:,1) = exp(-pi*x.^2);
-
-
-% Boundary conditions
-
-UN(1,:) = 0;
-UN(nx,:) = 0;
+% Initial and boundary conditions
+u(:,1) = sin(pi*xs);
+u(1,:) = 0;
+u(Nx,:) = 0;
 
 % Assigning values to each u(xi,ti)
-for j = 1:nt-1
-    for i = 2:nx-1
-        UN(i,j+1) = alpha*(UN(i+1,j)-2*UN(i,j)+ UN(i-1,j))+ UN(i,j);
+for j = 1:Nt-1
+    for i = 2:Nx-1
+        u(i,j+1) = alpha*(u(i+1,j)-2*u(i,j)+ u(i-1,j))+ u(i,j);
     end  
 end
 
 figure()
-contourf(UN,200,'linecolor','non')
-surf(t,x,UN)
+contourf(u,200,'linecolor','non')
+surf(ts,xs,u)
 title('1D Heat eq')
 xlabel('t')
 ylabel('x')
