7a776eefc464edf2079f87f24b4f38d24ed80a4e
Ben Stager <bstager@tulane.edu>
2023-04-09 22:42:32 -0400

    attempted to pass function handle in GMRES, failed

--- Time integration/integrators/IMEXSemiLinearEulerGMRES.m
+++ Time integration/integrators/IMEXSemiLinearEulerGMRES.m
@@ -8,16 +8,17 @@ ys(:,1) = y0;
 y = y0;
 dt = diff(tspan)/N;
 t = tspan(1);
-I = speye(length(y0));
+I = eye(length(y0));
 tol = 10^-2;
-h = 10^-5;
+h = 10^-8;
 
 tic
 for i = 1:N
-    J_yn = sparse(jac_FD(F,y,h));
+    J_yn = jac_FD(F,y,h);
     Jv = jac_ProdFD(F,y,y,h)';
-    [y,flag] = gmres(I-dt*J_yn,y + dt*(F(t,y)-Jv),...
-        size(I,1),tol);
+    A = I - J_yn;
+    [y,flag] = gmres(@(y) (I - J_yn)*y, y + dt*(F(t,y) - Jv),...
+        length(A),tol);
     ys(:,i+1) = y;
     t = t + dt;
 end
