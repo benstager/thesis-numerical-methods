0489d095caf5193d0b690f63b37bccc3cf952b31
Ben Stager <bstager@tulane.edu>
2023-04-13 14:44:57 -0400

    convergence/precision diagram algo for 3d stokes

---
+++ Time integration/error algorithms/stokes_t_convergence_precision_3d.m
@@ -0,0 +1,65 @@
+
+% Script plots the temporal error of centers for sequence of time steps
+% taken to solve highly viscous stokes flow
+clear all;
+
+% grid parameters
+n = 5;
+X = init_blob_3d(n);
+
+% reference fine solution, typical of power length(Nts) + 1
+Xv = reshape(X,[3*n,1]);
+tspan = [0,1];
+Nt_ref = 3^8;
+[Xvs,cpu] = IMEXSemiLinearEulerGMRES(@f_3d,tspan,Xv,Nt_ref);
+Xvi = Xvs(:,end);
+Xvi = reshape(Xvi,[3,length(Xvi)/3]);
+x_ref = Xvi(1,:);
+y_ref = Xvi(2,:);
+z_ref = Xvi(3,:);
+
+
+methods = {@euler, @heun, @IMEXSemiLinearEulerGMRES};
+Nts = 3.^(4:7);
+dts = diff(tspan)./Nts;
+
+error = zeros(length(Nts),length(methods));
+time = zeros(length(Nts),length(methods));
+
+% computing error at final time step for each cutoff position
+for i = 1:length(methods)
+    for j = 1:length(Nts)
+        [Xvs,cpu] = methods{i}(@f_3d,tspan,Xv,Nts(j));
+        Xvi = Xvs(:,end);
+        Xvi = reshape(Xvi,[3,length(Xvi)/3]);
+        x_coarse = Xvi(1,:);
+        y_coarse = Xvi(2,:);
+        z_coarse = Xvi(3,:);
+        error(j,i) = norm([norm(x_ref-x_coarse,2); ...
+            norm(y_ref-y_coarse,2);norm(z_ref-z_coarse,2)]);
+        time(j,i) = cpu;
+    end
+end
+
+getMethodName = @(f) functions(f).function;
+legend_entries = cellfun(getMethodName, methods, 'UniformOutput', false);
+
+
+% convergence diagram
+figure(1)
+loglog(dts,error,LineWidth=2.0); hold on;
+xlabel('step size (dt)');
+ylabel('error');
+title('Convergence Diagram for Stokes Flow');
+legend(legend_entries); legend box on;
+ylim([10^-5, 10^2]);
+
+% precision diagram
+figure(2)
+loglog(time,error,LineWidth=2.0);
+xlabel('time (sec)');
+ylabel('error');
+title('Precision Diagram for Stokes Flow');
+legend(legend_entries); legend box off;
+ylim([10^-5, 10^2]);
+        
